1b07709266c5b2097b749c1189fb9392c04c0163
Toi bui <syaoran2906@gmail.com>
2026-02-27 08:38:24 +0700

    fix import route

--- app/api/staffs/[id]/route.ts
+++ app/api/staffs/[id]/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { getServerSession } from "next-auth"
 import { authOptions } from '@/lib/auth'
 import { prisma } from '@/lib/prisma';
-import { StaffRole } from '@prisma/client/edge';
+import { StaffRole } from '@prisma/client'
 
 // GET single staff by ID
 export async function GET(req: Request, context: any) {
